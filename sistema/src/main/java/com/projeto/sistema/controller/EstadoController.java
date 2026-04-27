984d78f1abfbde20d1d76f28cec476dd8c01da9c
Ma1the3us <matheusrodrigues3@gmail.com>
2026-04-27 01:28:43 -0300

    iniciando a contrução do cadastro de estados

--- sistema/src/main/java/com/projeto/sistema/controller/EstadoController.java
+++ sistema/src/main/java/com/projeto/sistema/controller/EstadoController.java
@@ -1,9 +1,23 @@
 package com.projeto.sistema.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.projeto.sistema.model.Estado;
+import com.projeto.sistema.repositorios.EstadoRepositorio;
 
 @Controller
 public class EstadoController {
-    
 
+    @Autowired
+    private EstadoRepositorio estadoRepositorio;
+
+    @GetMapping("/cadastroEstado")
+    public ModelAndView cadastrar(Estado estado) {
+        ModelAndView mv = new ModelAndView("adiministrativo/estados/cadastro");
+        mv.addObject("estado", estado);
+        return mv;
+    }
 }
