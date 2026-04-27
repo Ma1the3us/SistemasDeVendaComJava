1166f78a065a1d41f19c64940bb63a839a8dbe91
Ma1the3us <matheusrodrigues3@gmail.com>
2026-04-27 00:28:21 -0300

    configurando o banco de dados

--- sistema/src/main/java/com/projeto/sistema/controller/principalController.java
+++ sistema/src/main/java/com/projeto/sistema/controller/principalController.java
@@ -5,9 +5,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class principalController {
-    @GetMapping("/administrativo")
+    @GetMapping("/adiministrativo")
     public String acessarPrincipal() {
-        return "administrativo/home";
+        return "adiministrativo/home";
     }
 
 }
